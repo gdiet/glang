e000f6f41fb18b8042271571d85d04f26e345fc8
georg <gdiet@users.noreply.github.com>
2022-02-14 17:06:09 +0100

    plan how to continue

--- src/main/scala/g2/Main.scala
+++ src/main/scala/g2/Main.scala
@@ -5,15 +5,21 @@ import scala.util.matching.Regex
 
 @main def main(): Unit =
   val script = """
-SAVE #0678 TO $0
-LOAD $0    TO A
-ADD  $0    TO A,B
 FUNCTION add_unsigned $x $y $z
 LOAD $x TO A
-RETURN
+ADD  $y TO A,B
+CLEAR F
+loop: CLEAR G
+0: IF B > 0 THEN SET F; 1..N: IF B > 0 THEN SET G
+IF NOT G THEN RETURN
+SHIFTLEFT B
+IF B > 0 THEN ADD #1 TO A,B
+GOTO loop
 END_FUNCTION
-CALL add_unsigned $0 $0 $1
-LOAD $0    TO A
+
+SAVE #0678 TO $0
+SAVE #9876 TO $1
+CALL add_unsigned $0 $1 $2
 // END
 """
 
