b46628d25a97593e30a36bb04a912fa968a3edd2
georg <gdiet@users.noreply.github.com>
2022-02-16 17:05:36 +0100

    different execution approach needed

--- src/main/scala/g2/Main.scala
+++ src/main/scala/g2/Main.scala
@@ -1,6 +1,7 @@
 package g2
 
 import scala.collection.mutable
+import scala.util.chaining.scalaUtilChainingOps
 import scala.util.matching.Regex
 
 @main def main(): Unit =
@@ -25,8 +26,9 @@ CALL add_unsigned $0 $1 $2
 """
 
   val lines = script.linesIterator.toVector
-  exec(lines, State())
   compile(lines)
+// TODO write an executor for the single actor, and one controller for the flow control
+//  exec(lines, State())
 
 def strip(line: String): String =
   line
@@ -52,146 +54,148 @@ object mat:
   /** after context replacement */
   val ADDR: Regex = raw"(\d+)".r
 
-def compile(lines: Vector[String]): Unit =
+def compile(lines: Vector[String]): Vector[Vector[String]] =
   println("\nCompilation:")
-  (0 until DIGITS).foreach { actor =>
+  (0 until DIGITS).map { actor =>
     println(s"\nActor $actor:")
-    lines.map(strip).filterNot(_.isEmpty).foreach(compile(actor))
-  }
+    lines.map(strip).map { line =>
+      compile(actor, line).tap(println)
+    }
+  }.toVector
 
-def compile(actor: Int)(line: String): Unit =
+def compile(actor: Int, line: String): String =
   object S { def unapply(string: String): Option[String] = Some(string.drop(1)) }
-  import mat._
+  import mat.*
   line match
 
     // SAVE #0678 TO $0
     case SAVE(CONSTANT(S(CONST(constant))), ADDRESS(S(ADDR(to)))) =>
-      println(raw"SAVE #${constant(actor)} TO $$$to")
+      raw"SAVE #${constant(actor)} TO $$$to"
 
     // LOAD $0 TO A
     case LOAD(ADDRESS(address), REGISTER(to)) =>
-      println(raw"LOAD $address TO $to")
+      raw"LOAD $address TO $to"
 
     // ADD $0 TO A,B
     case ADD(ADDRESS(S(ADDR(address))), REGISTER(to), REGISTER(overflow)) =>
-      println(raw"ADD $$$address TO $to,$overflow")
+      raw"ADD $$$address TO $to,$overflow"
 
     // FUNCTION add_unsigned $x $y $z
     case FUNCTION(NAME(name), rawParams) =>
-      println(raw"FUNCTION ${(name + rawParams).strip}")
+      raw"FUNCTION ${(name + rawParams).strip}"
 
     // CALL add_unsigned $0 $1 $2
     case CALL(NAME(name), rawParams) =>
-      println(raw"CALL ${(name + rawParams).strip}")
+      raw"CALL ${(name + rawParams).strip}"
 
     // RETURN
     case RETURN() =>
-      println(raw"RETURN")
+      raw"RETURN"
 
-    // RETURN
-    case END_FUNCTION() =>
-      /* ignore */
-
-    case other =>
-      println(s"** unknown command '$other' **")
-
-@annotation.tailrec
-def exec(lines: Vector[String], state: State): Unit =
-  if lines.size > state.line then
-    val line = lines(state.line)
-    state.out()
-    println(f"${state.line}%02d $line")
-    exec(lines, strip(line), state)
-    exec(lines, state)
-
-def exec(lines: Vector[String], currentLine: String, s: State): Unit =
-  import mat.*
-  import s.*
-  import matchers.*
-  currentLine match
-    case "" =>
-      line += 1
-
-    // SAVE #0678 TO $0
-    case SAVE(constant(constant), address(to)) =>
-      mem(to) = constant
-      line += 1
-
-    // LOAD $0 TO A
-    case LOAD(address(from), REGISTER(to)) =>
-      reg(to) = mem(from)
-      line += 1
-
-    // ADD $0 TO A,B
-    case ADD(address(from), REGISTER(to), REGISTER(overflow)) =>
-      val (a,b) = reg(to).zip(mem(from)).map(_+_).map(i => i % 10 -> i / 10).unzip
-      reg(to) = a
-      reg(overflow) = b
-      line += 1
-
-    // FUNCTION add_unsigned $x $y $z
-    case FUNCTION(NAME(name), rawParams) =>
-      val params = rawParams.split(raw"\s+").filterNot(_.isEmpty)
-      require(params.forall(_.startsWith("$")))
-      line += 1
-      functions += name -> FuncDefinition(line, params.toSeq)
-      while strip(lines(line)) != "END_FUNCTION"
-      do { println(s"FN ${lines(line)}"); line += 1 }
-      println(s"FN ${lines(line)}")
-      line += 1
-
-    // CALL add_unsigned $0 $1 $2
-    case CALL(NAME(name), rawParams) =>
-      require(stack.size < STACK, s"Stack overflow")
-      val params = rawParams.split(raw"\s+").filterNot(_.isEmpty).toSeq
-      require(params.forall(_.startsWith("$")))
-      val function = functions(name)
-      val functionParameters = function.parameters.zip(params).toMap
-      stack :+= Stackframe(function.line, functionParameters.to(mutable.Map))
-      Thread.sleep(500)
-
-    // RETURN
-    case RETURN() =>
-      require(stack.nonEmpty, s"Stack underflow")
-      stack = stack.dropRight(1)
-      line += 1
+    // END_FUNCTION and empty lines can be ignored
+    case END_FUNCTION() | "" =>
+      ""
 
     case other =>
-      println(s"** unknown command '$other' **")
-      line += 1
+      s"** unknown command '$other' **"
 
-class FuncDefinition(val line: Int, val parameters: Seq[String])
-
-class Stackframe(
-  var line: Int = 0,
-  val context: mutable.Map[String, String] = mutable.Map[String, String]()
-)
-
-class State:
-  def out(): Unit =
-    def memOut(m: Int): Int | String = if m < 0 then "#" else m
-    def l = line
-    def r = s"${reg("A").map(memOut).mkString} ${reg("B").map(memOut).mkString} ${reg("C").map(memOut).mkString} ${reg("D").map(memOut).mkString}"
-    def m = mem.map(_.map(memOut).mkString).mkString(" ")
-    println(s"L $l / R $r / M $m")
-  private def invalid = List.fill(DIGITS)(-1)
-
-  val mem: Array[List[Int]] = Array.fill(MEMORY)(invalid)
-  val reg: mutable.Map[String, List[Int]] = mutable.Map("A" -> invalid, "B" -> invalid, "C" -> invalid, "D" -> invalid)
-  var stack: Vector[Stackframe] = Vector(Stackframe())
-  val functions: mutable.Map[String, FuncDefinition] = mutable.Map()
-  def context: mutable.Map[String, String] = stack.last.context
-  def line: Int = stack.last.line
-  def line_=(line: Int): Unit = stack.last.line = line
-
-  def unapply(string: String): Option[String] = Some(context.getOrElse(string, string).drop(1))
-  val self: State = this
-  object matchers:
-    object constant:
-      def unapply(string: String): Option[List[Int]] = string match
-        case mat.CONSTANT(self(mat.CONST(raw))) => Some(raw.getBytes.toList.map(_ - 48))
-        case other => None
-    object address:
-      def unapply(string: String): Option[Int] = string match
-        case mat.ADDRESS(self(mat.ADDR(raw))) => raw.toIntOption
-        case other => None
+//@annotation.tailrec
+//def exec(lines: Vector[String], state: State): Unit =
+//  if lines.size > state.line then
+//    val line = lines(state.line)
+//    state.out()
+//    println(f"${state.line}%02d $line")
+//    exec(lines, strip(line), state)
+//    exec(lines, state)
+//
+//def exec(lines: Vector[String], currentLine: String, s: State): Unit =
+//  import mat.*
+//  import s.*
+//  import matchers.*
+//  currentLine match
+//    case "" =>
+//      line += 1
+//
+//    // SAVE #0678 TO $0
+//    case SAVE(constant(constant), address(to)) =>
+//      mem(to) = constant
+//      line += 1
+//
+//    // LOAD $0 TO A
+//    case LOAD(address(from), REGISTER(to)) =>
+//      reg(to) = mem(from)
+//      line += 1
+//
+//    // ADD $0 TO A,B
+//    case ADD(address(from), REGISTER(to), REGISTER(overflow)) =>
+//      val (a,b) = reg(to).zip(mem(from)).map(_+_).map(i => i % 10 -> i / 10).unzip
+//      reg(to) = a
+//      reg(overflow) = b
+//      line += 1
+//
+//    // FUNCTION add_unsigned $x $y $z
+//    case FUNCTION(NAME(name), rawParams) =>
+//      val params = rawParams.split(raw"\s+").filterNot(_.isEmpty)
+//      require(params.forall(_.startsWith("$")))
+//      line += 1
+//      functions += name -> FuncDefinition(line, params.toSeq)
+//      while strip(lines(line)) != "END_FUNCTION"
+//      do { println(s"FN ${lines(line)}"); line += 1 }
+//      println(s"FN ${lines(line)}")
+//      line += 1
+//
+//    // CALL add_unsigned $0 $1 $2
+//    case CALL(NAME(name), rawParams) =>
+//      require(stack.size < STACK, s"Stack overflow")
+//      val params = rawParams.split(raw"\s+").filterNot(_.isEmpty).toSeq
+//      require(params.forall(_.startsWith("$")))
+//      val function = functions(name)
+//      val functionParameters = function.parameters.zip(params).toMap
+//      stack :+= Stackframe(function.line, functionParameters.to(mutable.Map))
+//      Thread.sleep(500)
+//
+//    // RETURN
+//    case RETURN() =>
+//      require(stack.nonEmpty, s"Stack underflow")
+//      stack = stack.dropRight(1)
+//      line += 1
+//
+//    case other =>
+//      println(s"** unknown command '$other' **")
+//      line += 1
+//
+//class FuncDefinition(val line: Int, val parameters: Seq[String])
+//
+//class Stackframe(
+//  var line: Int = 0,
+//  val context: mutable.Map[String, String] = mutable.Map[String, String]()
+//)
+//
+//class State:
+//  def out(): Unit =
+//    def memOut(m: Int): Int | String = if m < 0 then "#" else m
+//    def l = line
+//    def r = s"${reg("A").map(memOut).mkString} ${reg("B").map(memOut).mkString} ${reg("C").map(memOut).mkString} ${reg("D").map(memOut).mkString}"
+//    def m = mem.map(_.map(memOut).mkString).mkString(" ")
+//    println(s"L $l / R $r / M $m")
+//  private def invalid = List.fill(DIGITS)(-1)
+//
+//  val mem: Array[List[Int]] = Array.fill(MEMORY)(invalid)
+//  val reg: mutable.Map[String, List[Int]] = mutable.Map("A" -> invalid, "B" -> invalid, "C" -> invalid, "D" -> invalid)
+//  var stack: Vector[Stackframe] = Vector(Stackframe())
+//  val functions: mutable.Map[String, FuncDefinition] = mutable.Map()
+//  def context: mutable.Map[String, String] = stack.last.context
+//  def line: Int = stack.last.line
+//  def line_=(line: Int): Unit = stack.last.line = line
+//
+//  def unapply(string: String): Option[String] = Some(context.getOrElse(string, string).drop(1))
+//  val self: State = this
+//  object matchers:
+//    object constant:
+//      def unapply(string: String): Option[List[Int]] = string match
+//        case mat.CONSTANT(self(mat.CONST(raw))) => Some(raw.getBytes.toList.map(_ - 48))
+//        case other => None
+//    object address:
+//      def unapply(string: String): Option[Int] = string match
+//        case mat.ADDRESS(self(mat.ADDR(raw))) => raw.toIntOption
+//        case other => None
