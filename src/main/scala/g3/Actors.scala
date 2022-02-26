3105c337a15f0d437e9ad086a5b3c615ff091d51
georg <gdiet@users.noreply.github.com>
2022-02-26 19:16:01 +0100

    handing on commands to Actors

--- src/main/scala/g3/Actors.scala
+++ src/main/scala/g3/Actors.scala
@@ -4,7 +4,7 @@ import java.util.concurrent.Executors
 import scala.concurrent.ExecutionContext
 import scala.util.matching.Regex
 
-class Actors(settings: Settings) extends AutoCloseable:
+class Actors(settings: Settings) extends AutoCloseable with ClassLogging:
   import settings.*
   private val threadPool = Executors.newFixedThreadPool(DIGITS)
   private implicit val executionContext: ExecutionContext = ExecutionContext.fromExecutor(threadPool)
@@ -13,14 +13,13 @@ class Actors(settings: Settings) extends AutoCloseable:
   var flags: Map[Char, Boolean] = Map('F' -> false, 'G' -> false, 'H' -> false, 'I' -> false)
   val actors: Seq[Actor] = (0 until DIGITS).map(Actor(settings, _, this)).toVector
 
+  def execute(command: String): Unit =
+    log.info(s"executing: $command")
+
 class Actor(settings: Settings, position: Int, actors: Actors):
   import settings.*
   val memory: Array[Int] = Array.fill(MEMORY)(INVALID)
   var registers: Map[Char, Int] = Map('A' -> INVALID, 'B' -> INVALID, 'C' -> INVALID, 'D' -> INVALID)
-  /** The head of the context contains the current context, it's a list because the tail contains the contexts
-    * of the calling stack frames. The current context is a "replace string A with string B before processing" map
-    * to support variables in calls. */
-  var context: List[Map[String, String]] = List(Map())
 
   def execute(command: String): Unit = command match
     case other => /* ignore */
@@ -32,9 +31,13 @@ object Manager:
   // FUNCTION (add_unsigned): ()
   // FUNCTION (add_unsigned): ($x $y $z)
   val functionDefinitionLine: Regex = "FUNCTION ([a-z_]+): *(.*)".r
+  // END_FUNCTION
   val endFunctionLine: Regex = "END_FUNCTION".r
-  val returnLine: Regex = "(|[a-z_]+: *)RETURN".r
+  // (loop:) (SOME_COMMAND)
+  // ()(SOME_COMMAND)
   val someCommandLine: Regex = "([a-z_]+:){0,1} *(.*)".r
+  // SAVE (#1234) TO ($3)
+  val saveConstantToAddress: Regex = "SAVE (.*) TO (.*)".r
 
 class Manager(lines: Seq[String], settings: Settings = Settings()) extends AutoCloseable with ClassLogging:
   import Manager.*
@@ -57,7 +60,9 @@ class Manager(lines: Seq[String], settings: Settings = Settings()) extends AutoC
     log.info(s"Pass 2: Execution, starting at line $startAtLineNumber")
     execute(startAtLineNumber, List((lines.size, Map())))
 
-  /** @param context Stack frames containing the line number to return to and the frame's context. */
+  /** @param context Stack frames containing the line number to return to and the frame's context.
+    *                A context is a "replace string A with string B before processing" map
+    *                to support variables in function calls.*/
   @annotation.tailrec
   private def execute(lineNumber: Int, context: List[(Int, Map[String, String])]): Unit =
     if lines.size > lineNumber then
@@ -74,8 +79,12 @@ class Manager(lines: Seq[String], settings: Settings = Settings()) extends AutoC
           log.info("empty line")
           execute(lineNumber + 1, context)
 
-        case someCommandLine(_, other) =>
-          log.info(s"other command: $other")
+        case someCommandLine(_, rawCommand) =>
+          log.info(s"command: $rawCommand")
+          val contextCommand = context.head._2.foldLeft(rawCommand){
+            case (command, (find, replace)) => command.replaceAll(s"\\Q$find\\E", replace)
+          }
+          actors.execute(contextCommand)
           execute(lineNumber + 1, context)
 
         case other =>
--- src/main/scala/g3/Rules.md
+++ src/main/scala/g3/Rules.md
@@ -6,6 +6,9 @@
 * "FUNCTION [name]:.*" with optional parameters defines a function.
 * Function definitions must be terminated by a matching "END_FUNCTION".
 * Function definitions may not be nested.
+* Function parameters are space separated.
+* When a function is called, the function parameters define the context for subsequent commands
+* The function context ends and the execution pointer returns when "RETURN" is executed.
 * "[name]:.*" defines a GOTO target with optional command appended.
 * Only the commands below can be appended to a GOTO target.
 
