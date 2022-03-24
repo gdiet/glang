6f1f891a57c6631e4522af0aaa67c24e449c1a75
georg <gdiet@users.noreply.github.com>
2022-03-24 16:53:35 +0100

    SET F TRUE compiled for execution by a single actor

--- src/main/scala/g4/Compiler.scala
+++ src/main/scala/g4/Compiler.scala
@@ -5,12 +5,15 @@ import scala.util.matching.Regex
 object Compiler:
   val log: org.slf4j.Logger = org.slf4j.LoggerFactory.getLogger(s"${getClass.getName}".replace("$", ""))
 
+  val SET_FLAG: Regex = """SET ([F-I]) (TRUE|FALSE) .*""".r
+
 class Compiler (settings: Settings, codeLines: Vector[String]):
   import Compiler.*
   import settings.DIGITS
 
   val code: Vector[Vector[String]] = codeLines.map(_ + " ").map { // Add space for simpler patterns
-    // prepared for case matching that will come eventually
-    other =>
+    case SET_FLAG(flag, value) =>
+      s"SET $flag $value" +: Vector.fill(DIGITS - 1)("NOP")
+    case other =>
       Vector.fill(DIGITS)(other.trim)
   }
